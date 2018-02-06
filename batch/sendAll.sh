70438876e8f766f5e226b6ebe40573704ea4bc67
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2018-02-06 20:20:30 +0100

    add lsf commands

---
+++ batch/sendAll.sh
@@ -0,0 +1,16 @@
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/20GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet20GeV -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/50GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet50GeV -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/100GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet100GeV  -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/200GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet200GeV  -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/500GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet500GeV  -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/1000GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet1000GeV  -q 1nh
+python submitJetClustering.py -i /eos/experiment/fcc/hh/simulation/samples/tracker_calobarrel/diJets/2000GeV/NTUP -n 100 --njobs 50 -o output/FccDiJet2000GeV  -q 1nh
+
+
+python submitJetClustering.py -o output/FccDiJet20GeV --collect
+python submitJetClustering.py -o output/FccDiJet50GeV --collect
+python submitJetClustering.py -o output/FccDiJet100GeV --collect
+python submitJetClustering.py -o output/FccDiJet200GeV --collect
+python submitJetClustering.py -o output/FccDiJet500GeV --collect
+python submitJetClustering.py -o output/FccDiJet1000GeV --collect
+python submitJetClustering.py -o output/FccDiJet2000GeV --collect
